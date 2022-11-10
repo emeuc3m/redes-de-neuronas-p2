edfe229fcf6b27ea299062a607c6566126efa9c6
David <myphonedavid@gmail.com>
2022-11-11 00:29:15 +0100

    separation_onlypandas funciona perfecti

--- separation_onlypand.py
+++ separation_onlypand.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
 FILE_PATH = './data_files/normalized-data.csv'
+TRAIN_PATH = './data_files/train-data-spl.csv'
+TEST_PATH = './data_files/test-data-spl.csv'
 df = pd.read_csv(FILE_PATH)
 
-print(df.groupby('NSP', group_keys=False).apply(lambda x: x.sample(frac=(2/3))))
+train = df.groupby('NSP', group_keys=False).apply(lambda x: x.sample(frac=(2/3)))
+test = df.drop(train.index)
+
+train.to_csv(TRAIN_PATH, index=False)
+test.to_csv(TEST_PATH , index=False)
